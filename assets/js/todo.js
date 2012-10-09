458ed690c046c98b4b2a0152a51b7db31c9754c2
Alvaro Flores <super.neeph@gmail.com>
2012-10-09 13:20:46 -0300

    Login test

--- assets/js/todo.js
+++ assets/js/todo.js
@@ -1,14 +1,38 @@
 $(document).ready(function() {
-    var $btn = $("#btn"),
-    	$txt = $("#txt"),
-    	$lab = $("#label");
+	var $formLogin = $("#loginForm"),
+		$btn = $formLogin.find("#btn");
 
-    $btn.on("click", function() {
-        $lab.html($txt.val());
-        return false;
-    });
+   	if($formLogin.length>0) {
+        var login = $formLogin.bind("invalid-form.validate",
+            function() {
+                // $msg.html("<div>Campos incompletos o rut invalido</div>");
+            }).validate({
+                errorPlacement: function(error, element) {},
+                submitHandler: function(form) {
+                    if(!$btn.hasClass("disabled")) {
+                        $btn.addClass("disabled");
+                        $.ajax({
+                            url: "http://www.clinicaucsancarlos.cl/agendamientoSAP/index.php?sec=log&do=in",
+                            type: "post",
+                            dataType: "json",
+                            data: $(form).serializeArray(),
+                            success: function(data) {
+                            	alert("ok");
+                                // $msg.html(data.MENSAJE);
+                                // if(data.ERROR == 0) { //OK
+                                //     window.location.reload(true);
+                                // } else {
+                                //     $btn.removeClass("disabled");
+                                // }
+                            },
+                            error: function() {
+                            	alert("error");
+                            }
+                        }); 
+                    }
+                    return false;
+                },
+                success: function(label) {}
+            });
+    } 
 });
-
-// $(document).ready(function() {
-
-// });
